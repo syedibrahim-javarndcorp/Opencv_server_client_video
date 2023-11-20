1840c306677950faa41e724b7d555260626d1863
Syed <syedibrahim-javarndcorp@gmail.com>
2023-11-20 18:19:29 +0530

    hi

---
+++ server.py
@@ -0,0 +1,55 @@
+import socket
+import cv2
+import pickle
+import struct
+import imutils
+import threading
+
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host_ip = '0.0.0.0'
+print('HOST IP:',host_ip)
+port = 9999
+socket_address = (host_ip,port)
+server_socket.bind(socket_address)
+server_socket.listen()
+print("Listening at",socket_address)
+
+def show_client(addr,client_socket):
+	try:
+		print('CLIENT {} CONNECTED!'.format(addr))
+		if client_socket: # if a client socket exists
+			data = b""
+			payload_size = struct.calcsize("Q")
+			while True:
+				while len(data) < payload_size:
+					packet = client_socket.recv(4*1024) # 4K
+					if not packet: break
+					data+=packet
+				packed_msg_size = data[:payload_size]
+				data = data[payload_size:]
+				msg_size = struct.unpack("Q",packed_msg_size)[0]
+				
+				while len(data) < msg_size:
+					data += client_socket.recv(4*1024)
+				frame_data = data[:msg_size]
+				data  = data[msg_size:]
+				frame = pickle.loads(frame_data)
+				text  =  f"CLIENT: {addr}"
+				frame = cv2.putText(frame, text, (20,20), cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,0),2)
+				cv2.imshow(f"FROM {addr}",frame)
+				key = cv2.waitKey(1) & 0xFF
+				if key  == ord('q'):
+					break
+			client_socket.close()
+	except Exception as e:
+		print(f"CLINET {addr} DISCONNECTED")
+		pass
+		
+while True:
+	client_socket,addr = server_socket.accept()
+	thread = threading.Thread(target=show_client, args=(addr,client_socket))
+	thread.start()
+	print("TOTAL CLIENTS ",threading.activeCount() - 1)
+	
+				
+
